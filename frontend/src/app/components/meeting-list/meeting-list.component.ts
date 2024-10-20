042370b1a3d0b070506713e77c90fbfbcc2f0904
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2024-10-20 14:07:50 +0200

    Add meeting list filter

--- frontend/src/app/components/meeting-list/meeting-list.component.ts
+++ frontend/src/app/components/meeting-list/meeting-list.component.ts
@@ -5,27 +5,41 @@ import { format } from 'date-fns';
 import { Meeting, Priority, Status } from '../../models/meeting.model';
 import { CommonModule } from '@angular/common';
 import { MeetingsService } from '../../services/meetings.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-meeting-list',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, FormsModule],
   templateUrl: './meeting-list.component.html',
   styleUrl: './meeting-list.component.css'
 })
 export class MeetingListComponent implements OnInit {
   meetings: Meeting[] = [];
+  filteredMeetings: Meeting[] = [];
+  selectedRoom: string = '';
 
   constructor(private meetingsService: MeetingsService) {}
 
   ngOnInit() {
     this.meetingsService.getMeetings().subscribe(
       {
-        next: (meetings: Meeting[]) => this.meetings = meetings,
+        next: (meetings: Meeting[]) => {
+          this.meetings = meetings;
+          this.filteredMeetings = meetings;
+        },
         error: (error: any) => console.error(error),
         complete: () => console.log('Meetings loaded')
       }
     );
+  }
+
+  filterMeetings(): void {
+    if (this.selectedRoom) {
+      this.filteredMeetings = this.meetings.filter(meeting => meeting.roomId.toString() === this.selectedRoom);
+    } else {
+      this.filteredMeetings = this.meetings;  // Show all meetings if no room is selected
+    }
   }
 
   deleteMeeting(id: number): void {
